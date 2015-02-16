2e93c49d486c047b2187dea40521a3350f7c2e1b
Jeff Beck <jeff.kitconcepts@gmail.com>
2015-02-16 10:37:10 -0500

    jd worked out error

--- etsy.js
+++ etsy.js
@@ -23,6 +23,74 @@ $(function(){
   };
 
 
+
+
+  $(".search-form").on("submit", function(event){
+
+    console.log("submit happened");
+    
+    event.preventDefault();
+    
+    var keywords  = $(".search-field").val();
+    
+    getData(keywords, function(data){
+
+      console.log("got data");
+    
+      var items = data.results;
+
+      $(".items_container").empty();
+
+      items.forEach(function(item){
+
+        console.log("appending item");
+
+        $(".items_container").append(myTemplate(item));
+
+      });
+
+    });
+  });
+
+
+  var myTemplate = _.template(
+    "<div class='single_item_container'>" +  
+      "<div class='item_box'>" +
+        "<div class='imgUrl'>" +
+          "<img src='<%= Images[0].url_570xN %>'>" +
+        "</div>" +
+        "<div class='name'>" +
+          "<a href='<%= url %>'><%= title %></a>" +
+        "</div>" + 
+        "<div class='seller'>" +
+          "<a href='<%= Shop.url %>'><%= Shop.shop_name %></a>" +
+        "</div>" +
+        "<div class='cost'>$<%= price %> <%= currency_code %></div>" +
+      "</div>" +
+    "</div>"  
+  ); 
+
+
+});   
+
+
+
+// var myTemplate = _.template(
+//       "<div class= 'product'>" +
+//         "<div class= 'name'><%= name %></div>" +
+//           "<div class= 'product-image'>" +
+//             "<img src ='<%= imgUrl %>'> " +
+//   "</div>" +
+//   "<div class='add-to-cart'>" +
+//       "<a href='<%= href %>'>Add to cart</a>" +
+//       "</div>" +
+//   "</div>"
+// );
+
+
+
+
+
 // $(function(){
 
 //   var getData = function(keywords, callback) {
@@ -78,52 +146,3 @@ $(function(){
   //     });
   //   });
   // });
-  $(".search-form").on("submit", function(event){
-    event.preventDefault();
-    var keywords  = $(".search-field").val();
-    getData(keywords, function(data){
-    var items = data.results;
-
-    $(".items_container").empty();
-
-    items.forEach(function(item){
-
-    $(".items_container").append(myTemplate(item));
-    
-      });
-    });
-  });
-
-
-// var myTemplate = _.template(
-//       "<div class= 'product'>" +
-//         "<div class= 'name'><%= name %></div>" +
-//           "<div class= 'product-image'>" +
-//             "<img src ='<%= imgUrl %>'> " +
-//   "</div>" +
-//   "<div class='add-to-cart'>" +
-//       "<a href='<%= href %>'>Add to cart</a>" +
-//       "</div>" +
-//   "</div>"
-// );
-
-
-  var myTemplate = _.template(
-    "<div class='items_container'>" +  
-      "<div class='item_box'>" +
-        "<div class='imgUrl'>" +
-          "<img src='<%= Images[0].url_570xN %>'>" +
-        "</div>" +
-        "<div class='name'>" +
-          "<a href='<%= url %>'><%= title %></a>" +
-        "</div>" + 
-        "<div class='seller'>" +
-          "<a href='<%= Shop.url %>'><%= Shop.shop_name %></a>" +
-        "</div>" +
-        "<div class='cost'>$<%= price %> <%= currency_code %></div>" +
-      "</div>" +
-    "</div>"  
-  ); 
-
-
-});        
